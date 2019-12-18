593da6b5db86c35e2f424ddf5b6b6d8e5c9f0673
yassine <yassine.tahri@outlook.fr>
2019-12-18 19:08:08 +0100

    done

---
+++ mygraphicsscene3.cpp
@@ -0,0 +1,6 @@
+#include "mygraphicsscene3.h"
+
+MyGraphicsScene3::MyGraphicsScene3()
+{
+
+}
---
+++ mygraphicsscene3.h
@@ -0,0 +1,11 @@
+#ifndef MYGRAPHICSSCENE3_H
+#define MYGRAPHICSSCENE3_H
+
+
+class MyGraphicsScene3
+{
+public:
+    MyGraphicsScene3();
+};
+
+#endif // MYGRAPHICSSCENE3_H
